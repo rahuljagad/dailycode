90dba71ad2e07e2cb9adacb86a89f221d3304cf5
Rahul Jagad <rahuljagad@gmail.com>
2024-01-26 09:42:20 -0800

    Solved: Jan 26nd 2024 Problem: 576

---
+++ 2024/january/01_26_2024_576.py
@@ -0,0 +1,26 @@
+'''
+576. Out of Boundary Paths
+Solved using recursion.
+'''
+
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        MOD = 10**9 + 7
+        ROWS, COLS = m, n
+        cache = {}
+
+        def find_path_recursion(row: int, col: int, remaining_moves: int):
+            if row < 0 or row == ROWS or col < 0 or col == COLS:
+                return 1
+            if remaining_moves == 0:
+                return 0
+            if (row, col, remaining_moves) in cache:
+                return cache[(row, col, remaining_moves)]
+            cache[(row, col, remaining_moves)] = (
+                find_path_recursion(row + 1, col, remaining_moves - 1) + 
+                find_path_recursion(row - 1, col, remaining_moves - 1) +
+                find_path_recursion(row, col + 1, remaining_moves - 1) +
+                find_path_recursion(row, col - 1, remaining_moves - 1)) % MOD
+            return cache[(row, col, remaining_moves)]
+        
+        return find_path_recursion(startRow, startColumn, maxMove)
